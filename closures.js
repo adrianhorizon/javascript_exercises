91c4d8881fb4a3e1d5055467e6baf8ca4db7b5e3
Adrian Caminos <camilod4568@gmail.com>
2018-07-10 12:57:25 -0500

    subiendo closures

---
+++ closures.js
@@ -0,0 +1,25 @@
+
+function saludarFamilia(apellido) {
+    // vamos a utilizar un closure
+    return function saludarMiembroFamilia(nombre) {
+        console.log(`Hola ${nombre} ${apellido}`)
+    }
+}
+const saludarGomez = saludarFamilia("Gomez")
+const saludarPerez = saludarFamilia("Perez") 
+
+
+saludarGomez("pedro")
+saludarGomez("luisa")
+saludarGomez("yo")
+
+saludarPerez("as")
+saludarPerez("asda")
+
+// funcion que permita prefijos para las palabras
+const makePreFixer = (prefijo) => imprimirPrefijo = (prefixer) => console.log(`${prefijo} ${prefixer}`)
+const PrefijoRe = makePreFixer("re")
+const prefijoIn = makePreFixer("in")
+
+PrefijoRe("bueno")
+prefijoIn("creible")
